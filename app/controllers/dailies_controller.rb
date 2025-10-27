74a256f1f5422881b59f5b62183d0ff4727ecc1a
Remi Tondeux <remi.tondeux@gmail.com>
2025-10-27 14:03:20 +0100

    test technique done

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -2,7 +2,7 @@ class DailiesController < ApplicationController
   before_action :set_daily, only: [ :show, :destroy ]
 
   def index
-    @dailies = Daily.all.order(date: :desc)
+    @dailies = Daily.all.order(date: :desc) # .all has preformance risks when lot's of dailies in database
   end
 
   def new
@@ -12,24 +12,41 @@ class DailiesController < ApplicationController
     file = params[:file]
 
     return redirect_to new_daily_path, notice: "Insérez un fichier." unless file
-    return redirect_to new_daily_path, notice: "le fichier doit être au format CSV." unless file.content_type == "text/csv"
+    return redirect_to new_daily_path, notice: "Le fichier doit être au format CSV." unless file.content_type == "text/csv"
 
     raw_datas = read_csv(file)
-    daily = Daily.new(date: raw_datas.first["datetime"], energy: daily_energy(raw_datas))
 
-    if daily.save!
-      raw_datas.each do |data|
-        raw_data = RawData.new(data.to_hash)
-        raw_data.daily_id = daily.id
-        raw_data.save
+    return redirect_to new_daily_path, notice: "Le fichier ne respecte pas le format attendu (3 colonnes)" unless three_column?(raw_datas)
+
+    dailies = split_in_dailies(raw_datas)
+
+    notice = "Données importées !"
+    i = 0
+
+    dailies.each do |daily|
+      daily = Daily.new(date: dailies[i].first["datetime"].split(" ").first)
+
+      if daily.save
+        dailies[i].each do |data|
+          raw_data = RawData.new()
+          raw_data.identifier = data["identifier"]
+          raw_data.datetime = data["datetime"].to_datetime.strftime("%F %H")
+          raw_data.energy = data["energy"]
+          raw_data.daily_id = daily.id
+          raw_data.save
+        end
+        daily.update(energy: daily.total_daily_energy)
+      else
+        notice = "Des données étaient déjà présentes, le complément a été sauvegardé."
       end
-      redirect_to dailies_path, notice: "Données importées !"
-    else
-      redirect_to new_daily_path, notice: "Les données sont déjà présentes dans notre base."
+      i += 1
     end
+
+    redirect_to dailies_path, notice: notice
   end
 
   def show
+    @production = @daily.energy_by_inverter
   end
 
   def destroy
@@ -39,13 +56,9 @@ class DailiesController < ApplicationController
 
   private
 
-  def set_daily
-    @daily = Daily.find(params[:id])
-  end
-
-  def daily_energy(raw_datas)
-    array = raw_datas.map{ |e| e["energy"].to_i }
-    array.sum
+  def how_many_days(raw_datas)
+    array = raw_datas.map { |e| e["datetime"].split(" ").first }
+    array.uniq!
   end
 
   def read_csv(file) # get a csv file, return an array of raw datas
@@ -54,5 +67,21 @@ class DailiesController < ApplicationController
       array << row.to_hash
     end
     array
+  end
+
+  def set_daily
+    @daily = Daily.find(params[:id])
+  end
+
+  def split_in_dailies(raw_datas) # manage file with multiples days production
+    days = how_many_days(raw_datas)
+
+    days.map { |day| raw_datas.select { |data| data["datetime"].split(" ").first == day } }
+  end
+
+  def three_column?(raw_datas)
+    array = raw_datas.map { |e| e.size }
+    array.reject! { |e| e != 3 }
+    array.size == raw_datas.size
   end
 end
--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,6 +1,40 @@
 class Daily < ApplicationRecord
   has_many :raw_datas, dependent: :destroy
 
-  validates :date, :energy, presence: true
+  validates :date, presence: true
   validates :date, uniqueness: true
+
+  # construct a bi-dimensionnal array to separate each inverter production within the daily production,
+  # also fill missing data with nil value
+  def energy_by_inverter
+    array = self.raw_datas
+    identifiers = find_identifiers(array)
+
+    organized_array = identifiers.map { |id| array.select { |data| data.identifier == id } }
+    filled_array = organized_array.map { |inverter| fill_with_nil_values(inverter) }
+  end
+
+  def total_daily_energy
+    array = self.raw_datas.map { |e| e["energy"].to_i }
+    array.sum
+  end
+
+  private
+
+  def fill_with_nil_values(array)
+    res = []
+    for i in 0..23 do
+      if array.any? { |e| e["datetime"].strftime("%k").to_i == i }
+      res << array.find { |e| e["datetime"].strftime("%k").to_i == i }
+      else
+        res << nil
+      end
+    end
+    res
+  end
+
+  def find_identifiers(datas)
+    arr = datas.map { |e| e.identifier }
+    arr.uniq
+  end
 end
